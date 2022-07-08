b4b541ff111afb6ac275e106a451ca59c3a43b83
franciowel <spina_francesco@blu.it>
2022-07-08 16:17:19 +0200

    creato header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- HEADER -->
+    <upperHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import upperHeader from './components/upperHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    upperHeader,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/commons.scss';
 </style>
---
+++ src/components/upperHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="background">
+        <img src="../assets/spotifylogo.png" alt="Spotify Logo">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'upperBanner',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/commons.scss';
+.background{
+    background-color: $primary;
+    height: 100px;
+    color: white
+}
+img {
+    width: 90px;
+    padding: 15px;
+}
+</style>

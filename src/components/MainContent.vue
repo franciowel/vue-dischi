317d76457e537a68b9d3a6761fd5f457c97d5566
franciowel <spina_francesco@blu.it>
2022-07-12 12:09:17 +0200

    esercizio completo con select

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -11,8 +11,19 @@
                         </div>
                     </div>
 
-                    <div v-else v-for="item, index in dataList" :key="index">
-                        <singleTrack :info="dataList" :item="item" />
+                    <div v-else>
+                        <div>
+                            <SelectBar @select="selectCategory" />
+                        </div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="d-flex flex-wrap">
+                                    <div v-for="item, index in filteredDataList" :key="index">
+                                        <singleTrack :info="dataList" :item="item" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
@@ -22,16 +33,19 @@
 
 </template>
 
+
 <script>
 import axios from 'axios';
-import singleTrack from './singleTrack';
+import singleTrack from './singleTrack.vue';
+import SelectBar from './SelectBar.vue';
 export default {
-    name: "MainContent",
+    name: "PageContainer",
     data() {
         return {
             url: "https://flynn.boolean.careers/exercises/api/array/music",
             dataList: [],
             isLoadin: true,
+            filteredDataList: [],
         };
     },
     created() {
@@ -43,11 +57,27 @@ export default {
                 this.dataList = result.data.response;
                 setTimeout(() => {
                     this.isLoadin = false;
+                    this.filteredDataList = this.dataList
                 }, 1000);
             });
+        },
+        selectCategory(select) {
+        this.filteredDataList = [];
+        for(let i = 0; i < this.dataList.length; i++) {
+            if (this.dataList[i].genre.toLowerCase() == select) {
+                this.filteredDataList.push(this.dataList[i]);
+            } 
         }
+        this.selectAll(select)
+        },
+        selectAll(select) {
+            if (select == 'all') {
+                this.filteredDataList = this.dataList;
+            }
+        }
+    
     },
-    components: { singleTrack }
+    components: { singleTrack, SelectBar }
 }
 </script>
 
---
+++ src/components/SelectBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="container">
+        <div class="text-center">
+            <select @change="$emit('search', selectCategory($event))" name="search" id="search" value="all">
+                <option value="all">All</option>
+                <option value="rock">Rock</option>
+                <option value="pop">Pop</option>
+                <option value="jazz">Jazz</option>
+                <option value="metal">Metal</option>
+            </select>
+        </div>    
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+    name: 'SelectBar',
+    data() {
+        return {
+            categoryId: '',
+        }
+    },
+    methods: {
+        selectCategory(event) {
+        this.categoryId = event.target.value
+        this.$emit('select', this.categoryId)
+    }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/commons.scss';
+div {
+    background-color: $main-color;
+}
+</style>
